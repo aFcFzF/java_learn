ca218f3c07306b9b21b2caa99e27921eccca1320
jialipeng.fe <jialipeng.fe@bytedance.com>
2020-10-13 23:55:43 +0800

    day01

---
+++ 01/01.java
@@ -0,0 +1,16 @@
+/**
+ * @file 01.java
+ * @author afcfzf(9301462@qq.com)
+ */
+
+class VariableTest {
+    public static void main(String[] args) {
+        int myNumber = 1;
+        byte myByte = -128;
+        short myShort = 9999;
+        float f1 = 1.1f;
+
+        System.out.println(f1);
+
+    }
+}
