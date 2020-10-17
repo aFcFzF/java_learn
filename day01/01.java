19ee447be6e2ddddf81852b321d4c6b5dd145427
afcfzf <9301462@qq.com>
2020-10-17 21:45:16 +0800

    1

--- day01/01.java
+++ day01/01.java
@@ -2,15 +2,21 @@
  * @file 01.java
  * @author afcfzf(9301462@qq.com)
  */
+import static java.lang.Math.*;
 
 class VariableTest {
     public static void main(String[] args) {
         int myNumber = 1;
-        byte myByte = -128;
+        byte myByte = -3;
         short myShort = 9999;
         float f1 = 1.1f;
 
-        System.out.println(f1);
+        float a = 1.2f;
 
+        System.out.println("结果是： " + Math.pow(3, 2));
+        System.out.println("结果2是： " + args);
+        System.out.println("结果3是： " + Byte.toUnsignedInt(myByte));
+        final int abb = 1;
+        System.out.println("结果4是:" + abb);
     }
 }
