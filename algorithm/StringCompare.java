0bcba3b6b1907861c23188f79ead866d4f1f2d60
afcffz <9301462@qq.com>
2020-12-19 00:35:51 +0800

    update

--- algorithm/StringCompare.java
+++ algorithm/StringCompare.java
@@ -1,30 +1,48 @@
+// method1
 class StringCompare {
     public char findTheDifference(String s, String t) {
         java.util.Map<Character, Integer> map = new java.util.HashMap<Character, Integer>();
 
-        for (int i = 0; i < s.length(); i++) {
-            char item = s.charAt(i);
-            map.put(item, !map.containsKey(item) ? 1 : map.get(item) + 1);
+        for (char ch: s.toCharArray()) {
+            map.put(ch, !map.containsKey(ch) ? 1 : map.get(ch) + 1);
         }
 
-        for (int i = 0; i < t.length(); i++) {
-            char item = t.charAt(i);
-
-            if (!map.containsKey(item)) {
-                return t.charAt(i);
+        for (char ch: t.toCharArray()) {
+            if (!map.containsKey(ch)) {
+                return ch;
             }
 
-            if (map.containsKey(item) && map.get(item) == 0) {
-                return t.charAt(i);
+            if (map.containsKey(ch) && map.get(ch) == 0) {
+                return ch;
             }
 
-            map.put(item, map.get(item) - 1);
+            map.put(ch, map.get(ch) - 1);
         }
 
         return 'a';
     }
 
     public static void main(String[] params) {
-        new StringCompare().findTheDifference("abc", "abcd");
+        Character m1 = new StringCompare().findTheDifference("abc", "abcd");
+        Character m2 = new StringCompareM2().diff("abc", "abcd");
+        Character m3 = new StringCompareM3().diff("abc", "abcd");
+        System.out.println("m1: " + m1);
+        System.out.println("m2: " + m2);
+        System.out.println("m3: " + m3);
+    }
+}
+
+class StringCompareM2 {
+    public char diff(String s, String t) {
+        return (char)(s + t).chars().reduce(0, (a, b) -> a ^ b);
+    }
+}
+
+class StringCompareM3 {
+    public char diff(String s, String t) {
+        final int less = s.chars().reduce(0, (a, b) -> a + b);
+        final int more = t.chars().reduce(0, (a, b) -> a + b);
+
+        return (char)(more - less);
     }
 }
