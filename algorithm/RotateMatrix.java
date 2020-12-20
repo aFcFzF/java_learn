d53a440d2123ef561b964f949a59cdc0fffe7d1b
afcffz <9301462@qq.com>
2020-12-20 23:38:47 +0800

    rotate

---
+++ algorithm/RotateMatrix.java
@@ -0,0 +1,28 @@
+/**
+ * @file RotateMatrix.java
+ * @author afcfzf(9301462@qq.com)
+ */
+
+// 暴力
+public class RotateMatrix {
+    public static void main(String[] params) {
+        final RotateMatrix m1 = new RotateMatrix();
+        final int[][] mtx = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        System.out.println(mtx);
+    }
+
+    public void rotate(int[][] mtx) {
+        int last = mtx[0].length - 1;
+        int[][] mtxNew = new int[last + 1][last + 1];
+
+        for (int i = last; i >= 0; i--) {
+            for (int j = 0; j <= last; j++) {
+                mtxNew[i][last - j] = mtx[j][i];
+            }
+        }
+
+        for (int i = 0; i <= last; i++) {
+            for (int j = 0; j <= last; j++) mtx[i][j] = mtxNew[i][j];
+        }
+    }
+}
