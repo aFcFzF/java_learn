2a01485e362feaf523cdee28d26617e9b6e05d8c
afcffz <9301462@qq.com>
2021-01-21 23:03:13 +0800

    update: entity

--- spring-boot/src/main/java/com/example/demo/DemoApplication.java
+++ spring-boot/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
+import java.util.*;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+
+		JdbcTemplate jdbc = context.getBean(JdbcTemplate.class);
+		List<Map<String, Object>> result = jdbc.queryForList("select * from posts");
+		System.out.println("===== aabbccddeeefff ====== ");
+		System.out.println(result);
 	}
 
 }
