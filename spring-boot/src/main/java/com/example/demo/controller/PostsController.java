6defec1e5e968d200e3d989e442dafd6be8013a4
afcffz <9301462@qq.com>
2021-01-22 23:05:40 +0800

    update

--- spring-boot/src/main/java/com/example/demo/controller/PostsController.java
+++ spring-boot/src/main/java/com/example/demo/controller/PostsController.java
@@ -1,11 +1,17 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
+import com.example.demo.entity.Posts;
 import com.example.demo.service.PostsService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +22,13 @@ public class PostsController {
     @Autowired
     private PostsService postsService;
 
+    @RequestMapping("/getPosts")
+    @ResponseBody
+    public Posts getPosts() {
+        Posts posts = postsService.getPosts();
+        System.out.println(posts);
+
+        return posts;
+    }
 
 }
