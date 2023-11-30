4d742611d3e79a1fb8002539347cc1369f356ace
afcfzf <9301462@qq.com>
2023-11-30 12:20:30 +0800

    PlayingWithDigits

---
+++ codewars/PlayingWithDigits.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class PlayingWithDigits {
+  public static void main(String[] args) {
+    PlayingWithDigits ins = new PlayingWithDigits();
+    System.out.println(ins.digPow(89, 1));
+    System.out.println(ins.digPow(92, 1));
+    System.out.println(ins.digPow(46288, 3));
+  }
+
+  private long digPow(int n, int p) {
+    String[] strList = Integer.toString(n).split("");
+    long[] intList = Arrays.asList(strList).stream().mapToLong(Integer::new).toArray();
+    long sum = 0;
+    for (int i = 0; i < intList.length; i++) {
+      long num = intList[i];
+      sum += Math.pow(num, i + p);
+    }
+
+    return sum % n == 0 ? sum / n : -1;
+  }
+}
