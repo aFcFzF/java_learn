76cd235866f12a89cb8ea4c666cb204a80ec29bd
afcfzf <9301462@qq.com>
2024-02-14 21:28:28 +0800

    fix: image

--- java_docker_base/assets/init.sh
+++ java_docker_base/assets/init.sh
@@ -2,8 +2,16 @@
 
 tar -zxf ./TencentKona-17.0.10.b1-jdk_linux-x86_64.tar.gz
 
-BASHRC_PATH="/root/.bashrc"
-echo "export JAVA_HOME=/data/software/TencentKona-17.0.10.b1;" >>$BASHRC_PATH
-source $BASHRC_PATH
-echo "export PATH=$JAVA_HOME/bin:$PATH;" >>$BASHRC_PATH
-source $BASHRC_PATH
+## declare an array variable
+declare -a arr=("/root/.bashrc" "/bin/profile")
+
+## now loop through the above array
+for i in "${arr[@]}"
+do
+  echo "$i"
+  BASHRC_PATH="$i"
+  echo "export JAVA_HOME=/data/software/TencentKona-17.0.10.b1;" >>$BASHRC_PATH
+  source $BASHRC_PATH
+  echo "export PATH=$JAVA_HOME/bin:$PATH;" >>$BASHRC_PATH
+  source $BASHRC_PATH
+done
