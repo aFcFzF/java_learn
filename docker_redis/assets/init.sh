cb55e79f0e98eefad380690d2ff0b98b2f277b70
afcfzf <9301462@qq.com>
2024-03-29 11:48:56 +0800

    fix: deploy mysql

--- docker_redis/assets/init.sh
+++ docker_redis/assets/init.sh
@@ -2,10 +2,15 @@
 
 if [ -z "$(ls -A /data/config/mysql/db)" ]; then
   /etc/init.d/mariadb setup
+  mysql_install_db
   echo "mysql initialed"
 fi
 
+# 启动mysql
 rc-service mariadb start
+
+# 启动redis
+redis-server /data/config/redis/redis.conf --appendonly yes
 
 if [[ $1 == "-d" ]]; then
   while true; do sleep 1000; done
