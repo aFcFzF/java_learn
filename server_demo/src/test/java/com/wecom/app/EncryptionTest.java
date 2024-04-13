ae03dfc150a7365cb8d9ab4bd42f3a99fb3091c8
afcfzf <9301462@qq.com>
2024-04-13 21:44:10 +0800

    fix

---
+++ server_demo/src/test/java/com/wecom/app/EncryptionTest.java
@@ -0,0 +1,35 @@
+package com.wecom.app;
+
+import java.io.IOException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.demo.app.App;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@SpringBootApplication
+public class EncryptionTest {
+  @Value("${spring.application.name}")
+  private String applicationName;
+
+  @Value("${classpath}")
+  private String classPath;
+
+  private String getClassPath() throws IOException {
+    ClassPathResource resource = new ClassPathResource("application.properties");
+    return resource.getURI().toString();
+  }
+
+  @Test
+  public void testApplicationName() throws IOException {
+    System.out.println("Application Name: " + applicationName);
+    String classPath = getClassPath();
+    System.out.println("classPath: " + classPath);
+  }
+}
