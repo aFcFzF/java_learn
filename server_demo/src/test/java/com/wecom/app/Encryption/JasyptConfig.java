b5d613a698fb01a3544a9b6896f16f2ff17e48d9
afcfzf <9301462@qq.com>
2024-04-15 14:07:25 +0800

    fix

---
+++ server_demo/src/test/java/com/wecom/app/Encryption/JasyptConfig.java
@@ -0,0 +1,19 @@
+package com.wecom.app.Encryption;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "jasypt.encryptor")
+public class JasyptConfig {
+  private String ivGeneratorClassname;
+
+  private String algorithm;
+
+  private String password;
+}
+
+
--- server_demo/src/test/java/com/wecom/app/EncryptionTest.java
+++ server_demo/src/test/java/com/wecom/app/EncryptionTest.java
@@ -1,13 +1,22 @@
 package com.wecom.app;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertSame;
+
 import java.io.IOException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.junit4.SpringRunner;
+import com.wecom.app.Encryption.JasyptConfig;
+import org.jasypt.digest.*;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,15 +25,30 @@ public class EncryptionTest {
   @Value("${spring.application.name}")
   private String applicationName;
 
+  @Autowired
+  private JasyptConfig jasyptConfig;
+
 
   private String getClassPath() throws IOException {
     ClassPathResource resource = new ClassPathResource("application.properties");
     return resource.getURI().toString();
   }
 
+  @Bean
+  public StringEncryptor stringEncryptor() {
+    StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+    encryptor.setPassword(jasyptConfig.getPassword());
+    encryptor.setAlgorithm(jasyptConfig.getAlgorithm());
+    return encryptor;
+  }
+
   @Test
-  public void testApplicationName() throws IOException {
+  public void testDecryption() throws IOException {
     System.out.println("Application Name: " + applicationName);
+    StringEncryptor encryptor = stringEncryptor();
+    String deString = encryptor.decrypt("QJNEs8WfhnOSSMMXqQgkVg==");
+    assertSame(deString, "pass");
+    System.out.println("jasypt Pwd: " + jasyptConfig);
     String classPath = getClassPath();
     System.out.println("classPath: " + classPath);
   }
