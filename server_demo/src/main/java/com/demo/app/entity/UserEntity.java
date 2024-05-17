11df958c04e35dff11d764bb133a4e9b21d9354e
afcfzf <9301462@qq.com>
2024-05-17 21:28:51 +0800

    fix

--- server_demo/src/main/java/com/demo/app/entity/UserEntity.java
+++ server_demo/src/main/java/com/demo/app/entity/UserEntity.java
@@ -11,6 +11,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Data;
 import lombok.ToString;
@@ -49,11 +50,13 @@ public class UserEntity {
   /**
    * 创建时间
    */
+  @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
   private LocalDateTime createTime;
 
   /**
    * 更新时间
    */
+  @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
   private LocalDateTime updateTime;
 
   /**
--- server_demo/src/test/java/com/wecom/app/UserTest.java
+++ server_demo/src/test/java/com/wecom/app/UserTest.java
@@ -4,15 +4,9 @@
  */
 
 package com.wecom.app;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.demo.app.App;
-import com.demo.app.entity.UserEntity;
-import com.demo.app.mapper.UserMapper;
 import com.demo.app.modules.common.util.FilePathInfo;
 import com.demo.app.modules.common.util.FileUtil;
 
