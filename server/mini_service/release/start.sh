dd27e90b5a49a2646b3ab16a11b59186541fd8d2
afcfzf <9301462@qq.com>
2025-02-26 19:18:06 +0800

    fix: app启动路径

--- server/mini_service/release/start.sh
+++ server/mini_service/release/start.sh
@@ -3,4 +3,4 @@
 cat ~/.bashrc
 java -version
 # source ~/.bashrc
-java -jar -Dspring.profiles.active=prod ./mini_service-1.0.jar || true
+java -jar -Dspring.profiles.active=prod ./mini_service.jar || true
